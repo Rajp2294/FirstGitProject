ce653dd7d298f521ae8cfef0048cf55aa6303b2e
Raj Parekh <rajparekh2294@gmail.com>
2023-08-13 18:06:42 +0530

    Added a for loop in both the files

--- greetings.java
+++ greetings.java
@@ -2,6 +2,8 @@ class Greetings{
 	public static void main(String[] args){
 		System.out.println("This is a new file");
 		System.out.println("Hey how are you??");
+		for(int i = 0; i < 5; i++){
+			System.out.println(i);
 	}
 }
 
--- hello_world.java
+++ hello_world.java
@@ -2,6 +2,9 @@
 class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello World!!);
+		for(int i = 0; i <= 5; i++){
+			System.out.println(i);
+		}
 	}
 }
 
